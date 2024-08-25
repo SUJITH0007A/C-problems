1373d6e195bfce5c6a56498b666bc3177f80b2ab
sujith mogaveera <135199420+SUJITH0007A@users.noreply.github.com>
2024-08-25 11:21:19 +0530

    Create area.c
    c programm to find the area of circle

---
+++ area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // this programm is to find the area of circle & volume of cylinder
+    float radius = 2.4;
+    float height = 5.3;
+    printf("The area of the circle is : %f\n", 3.14 * radius * radius);
+    printf("The volume of the cylinder is %f:\n ", 3.14 * (radius * radius) * height);
+
+    return 0;
+}
